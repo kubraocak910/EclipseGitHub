9fd7abf07c3fab9f49aae58e828a121880b632bb
Kubra <147837083+kubraocak910@users.noreply.github.com>
2025-01-07 15:50:05 +0300

    Update VeritabaniYardimcisi.java

--- src/sayfalar/VeritabaniYardimcisi.java
+++ src/sayfalar/VeritabaniYardimcisi.java
@@ -12,7 +12,7 @@ import java.util.Date;
 public class VeritabaniYardimcisi {
     private static final String URL = "jdbc:mysql://localhost:3306/ayka";
     private static final String USER = "root";
-    private static final String PASSWORD = "Hk795104199";
+    private static final String PASSWORD = "*********";
     
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
